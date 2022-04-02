ce1aee437de731bb7e671e4fdfe4886beab62b21
Anastasia <nastyapashkevich10@gmail.com>
2022-04-02 18:45:35 +0200

    feat: post creation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyAD7-nJB_D0eYP2GL24XaqUG1lbcbNlfwc'
+  apiKey: 'AIzaSyAD7-nJB_D0eYP2GL24XaqUG1lbcbNlfwc',
+  fbDbUrl: 'https://angular-blog-9e66a-default-rtdb.firebaseio.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyAD7-nJB_D0eYP2GL24XaqUG1lbcbNlfwc'
+  apiKey: 'AIzaSyAD7-nJB_D0eYP2GL24XaqUG1lbcbNlfwc',
+  fbDbUrl: 'https://angular-blog-9e66a-default-rtdb.firebaseio.com/'
 };
 
 /*
